fc6188993d77af960c430a638001a671008a3621
dwqs <461147874@qq.com>
2018-01-27 14:14:04 +0800

    refactor: Add empty component

---
+++ src/components/empty-icon.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="v2-table__empty-icon">
+        <div class="v2-table__icon--inner">
+            <span class="inner-rect"></span>
+            <span class="inner-horizontal-line inner-line1"></span>
+            <span class="inner-horizontal-line inner-line2"></span>
+            <span class="inner-horizontal-line inner-line3"></span>
+            <span class="inner-circle"></span>
+        </div>
+    </div>
+</template>
+
+<script>
+    import '../style/icon.less';
+
+    export default {
+        name: 'empty-icon'
+    };
+</script>
